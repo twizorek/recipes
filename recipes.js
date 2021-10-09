e7fe6de037a1560236d0d00f36f63386d14f1079
twizorek <36463249+twizorek@users.noreply.github.com>
2021-10-09 10:41:11 -0400

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,6 @@
+var cowVar = ['Beef Stroganoff', 'Ghoulash', 'Chili', 'Sloppy Joes', 'RR Suffed Pizza', 'Shepherds Pie', 'Pot Pie', 'Cheesebuger casserole with raveolis', 'Pot Roast', 'Shredded beef sandwiches (with apple cider)', 'Cornedbeef', 'Tacos', 'Enchaldas', 'Baked Bean Casserole', 'Copy Cat White Castles', 'Chili Dog Sauce'];
+
+
+console.log(cowVar);
+		
+	
